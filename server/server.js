c3f2a36b45250f98e0e3033d0d0cb184407e60d2
NHoxhaj <hoxhajnoela98@gmail.com>
2024-08-21 22:31:20 +0200

    deploy

--- server/server.js
+++ server/server.js
@@ -4,8 +4,26 @@ const socketIo = require('socket.io');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
-
+const path=require('path')
 const app = express();
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname  , "../client/dist");
+
+app.use(express.static(buildPath))
+
+app.get("/*", function(req, res){
+
+    res.sendFile(
+        path.join(__dirname, "../client/dist/index.html"),
+        function (err) {
+          if (err) {
+            res.status(500).send(err);
+          }
+        }
+      );
+
+})
+
 const PORT = process.env.PORT || 8000;
 
 app.use(cookieParser());
@@ -15,10 +33,15 @@ app.use(express.urlencoded({ extended: true }));
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true,
   }
+});
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 io.on('connection', (socket) => {
